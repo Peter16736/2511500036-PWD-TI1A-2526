6b9b6be6afb5910f804cfcfa7807c05a64337d3e
Peter Imanuel <2511500036@mahasiswa.atmaluhur.ac.id>
2025-11-03 16:53:35 +0700

    index.php

--- pertemuan-06/index.php
+++ pertemuan-06/index.php
@@ -253,58 +253,58 @@
             $nilaiUAS3 = "";
             $nilaiUAS4 = "";
             $nilaiUAS5 = "";
-        ?>
+        
         $namaMatkul = ["Algoritma dan Struktur Data", "Agama", "Matematika Diskrit", "Basis Data", "Pemrograman Web Dasar"];
-$sks        = [4, 2, 3, 3, 3];
-$hadir      = [90, 70, 80, 75, 69];
-$tugas      = [60, 50, 70, 80, 80];
-$uts        = [80, 60, 75, 85, 90];
-$uas        = [70, 80, 70, 90, 100];
+        $sks        = [4, 2, 3, 3, 3];
+        $hadir      = [90, 70, 80, 75, 69];
+        $tugas      = [60, 50, 70, 80, 80];
+        $uts        = [80, 60, 75, 85, 90];
+        $uas        = [70, 80, 70, 90, 100];
 
-$totalBobot = 0;
-$totalSKS   = 0;
+        $totalBobot = 0;
+        $totalSKS   = 0;
 
-echo "<section id='ipk'>";
-echo "<h2>Nilai Saya</h2>";
+        echo "<section id='ipk'>";
+        echo "<h2>Nilai Saya</h2>";
 
-for ($i = 0; $i < count($namaMatkul); $i++) {
+        for ($i = 0; $i < count($namaMatkul); $i++) {
 
-    $nilaiAkhir = (0.1 * $hadir[$i]) + (0.2 * $tugas[$i]) + (0.3 * $uts[$i]) + (0.4 * $uas[$i]);
+            $nilaiAkhir = (0.1 * $hadir[$i]) + (0.2 * $tugas[$i]) + (0.3 * $uts[$i]) + (0.4 * $uas[$i]);
 
-    if ($hadir[$i] < 70) {
-        $grade = "E";
-        $mutu = 0.00;
-    } elseif ($nilaiAkhir >= 90) {
-        $grade = "A";
-        $mutu = 4.00;
-    } elseif ($nilaiAkhir >= 80) {
-        $grade = "A-";
-        $mutu = 3.70;
-    } elseif ($nilaiAkhir >= 75) {
-        $grade = "B+";
-        $mutu = 3.30;
-    } elseif ($nilaiAkhir >= 70) {
-        $grade = "B";
-        $mutu = 3.00;
-    } elseif ($nilaiAkhir >= 65) {
-        $grade = "B-";
-        $mutu = 2.70;
-    } elseif ($nilaiAkhir >= 60) {
-        $grade = "C+";
-        $mutu = 2.30;
-    } elseif ($nilaiAkhir >= 55) {
-        $grade = "C";
-        $mutu = 2.00;
-    } elseif ($nilaiAkhir >= 50) {
-        $grade = "C-";
-        $mutu = 1.70;
-    } elseif ($nilaiAkhir >= 35) {
-        $grade = "D";
-        $mutu = 1.00;
-    } else {
-        $grade = "E";
-        $mutu = 0.00;
-    }
+        if ($hadir[$i] < 70) {
+            $grade = "E";
+            $mutu = 0.00;
+        } elseif ($nilaiAkhir >= 90) {
+            $grade = "A";
+            $mutu = 4.00;
+        } elseif ($nilaiAkhir >= 80) {
+            $grade = "A-";
+            $mutu = 3.70;
+        } elseif ($nilaiAkhir >= 75) {
+            $grade = "B+";
+            $mutu = 3.30;
+        } elseif ($nilaiAkhir >= 70) {
+            $grade = "B";
+            $mutu = 3.00;
+        } elseif ($nilaiAkhir >= 65) {
+            $grade = "B-";
+            $mutu = 2.70;
+        } elseif ($nilaiAkhir >= 60) {
+            $grade = "C+";
+            $mutu = 2.30;
+        } elseif ($nilaiAkhir >= 55) {
+            $grade = "C";
+            $mutu = 2.00;
+        } elseif ($nilaiAkhir >= 50) {
+            $grade = "C-";
+            $mutu = 1.70;
+        } elseif ($nilaiAkhir >= 35) {
+            $grade = "D";
+            $mutu = 1.00;
+        } else {
+            $grade = "E";
+            $mutu = 0.00;
+        }
 
     $status = in_array($grade, ["A", "A-", "B+", "B", "B-", "C+", "C", "C-"]) ? "LULUS" : "GAGAL";
 
@@ -332,6 +332,7 @@ echo "<h3>Total Bobot = " . number_format($totalBobot, 2) . "</h3>";
 echo "<h3>Total SKS = $totalSKS</h3>";
 echo "<h3>IPK = " . number_format($IPK, 2) . "</h3>";
 echo "</section>";
+?>
     </section>
  </main>
 
