36c297ee3b219d9c8448beb7b163bd406dcfa1a6
Peter Imanuel <2511500036@mahasiswa.atmaluhur.ac.id>
2025-11-05 12:12:35 +0700

    update README.md pertemuan-7

--- pertemuan-07/get.php
+++ pertemuan-07/get.php
@@ -1,3 +1,10 @@
+<?php
+  session_start();
+  $sesname = $_SESSION["nama"];
+  $sesemail = $_SESSION["email"];
+  $sespesan = $_SESSION["pesan"];
+  
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -82,6 +89,12 @@
         <button type="submit">Kirim</button>
         <button type="reset">Batal</button>
       </form>
+      <p>Terima kasih sudah menghubungi kami:
+        <label>Nama: <strong><?php echo $sesname; ?></strong></label>
+        <label>Email: <strong><?php echo $sesemail; ?></strong></label>
+        <label>Pesan: <strong><?php echo $sespesan; ?></strong></label>
+        
+      </p>
     </section>
   </main>
 
