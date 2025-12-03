6395ec34836680db842d5bd6f0e2abc44485fc33
Peter Imanuel <2511500036@mahasiswa.atmaluhur.ac.id>
2025-12-03 12:25:07 +0700

    membuat file baru read.php dan mengisi kodenya

--- pertemuan-10/README.md
+++ pertemuan-10/README.md
@@ -7,7 +7,7 @@ Nama:Peter Imanuel
   <li>menambahkan file baru koneksi.php dan mengisi kodenya</li>
   <li>membuat database baru di phpmyadmin</li>
   <li>membuat tbl_tamu di phpMyAdmin</li>
-  <li></li>
+  <li>membuat file baru read.php dan mengisi kodenya</li>
   <li></li>
   <li></li>
   <li></li>
--- pertemuan-10/koneksi.php
+++ pertemuan-10/koneksi.php
@@ -2,10 +2,11 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db = "db_pwd2025"
+$db   = "db_pwd2025";
 
-$conn = mysqli_connect($host, $user, $pass, $db)
+$conn = mysqli_connect  ($host, $user, $pass, $db);
 
 if (!$conn) {
-    die("Koneksi gagal: " . mysqli_connect_error());
+    die("Koneksi gagal: " . mysqli_connect_error());    
 }
+?>
