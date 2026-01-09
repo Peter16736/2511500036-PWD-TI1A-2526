06cbc9ceef47989e8a98d7362ef24ad2cb23b3cc
Peter Imanuel <2511500036@mahasiswa.atmaluhur.ac.id>
2026-01-10 01:11:23 +0700

    main document

--- pertemuan-15/index.php
+++ pertemuan-15/index.php
@@ -144,8 +144,6 @@ require_once __DIR__ . '/fungsi.php';
         </div>
       <?php endif; ?>
 
-      <form action="proses.php" method="POST">
-
         <label for="txtNama"><span>Nama:</span>
           <input type="text" id="txtNama" name="txtNama" placeholder="Masukkan nama"
             required autocomplete="name"
